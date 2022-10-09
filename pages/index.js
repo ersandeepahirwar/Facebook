0fafd9c756c7eb3a0e35c63e1e60282ff8d9356f
ersandeepahirwar <codeysandeep@gmail.com>
2022-10-10 01:45:30 +0530

    Setting up the next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import { getSession } from "next-auth/client";
+
 import Header from "../components/Header/Header";
 
 export default function Home() {
@@ -24,4 +26,13 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
 }
