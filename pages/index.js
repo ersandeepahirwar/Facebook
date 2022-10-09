2b04a93e9baa6cda569ba7f8a5cc520dfb9a622a
ersandeepahirwar <codeysandeep@gmail.com>
2022-10-09 21:42:58 +0530

    Intialized the facebook 2.0 project

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+        <meta
+          name="description"
+          content="Facebook 2.0 built with Next.js by Codey Sandeep"
+        />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+    </div>
+  );
+}
