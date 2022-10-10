6f843b7436b44ce86667af4d086824254c7aa26f
ersandeepahirwar <codeysandeep@gmail.com>
2022-10-11 02:20:12 +0530

    Setting up the Image uploading functionality

--- components/Feed/components/Publisher/Publisher.js
+++ components/Feed/components/Publisher/Publisher.js
@@ -1,16 +1,76 @@
 import Image from "next/image";
 
+import { useRef, useState } from "react";
 import { useSession } from "next-auth/client";
 
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
-
 import { EmojiHappyIcon } from "@heroicons/react/outline";
+
+import {
+  serverTimestamp,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+
+import { ref, getDownloadURL, uploadString } from "firebase/storage";
+
+import { database, storage } from "../../../../firebase";
 
 const Publisher = () => {
   const [session] = useSession();
 
-  const sendPost = (event) => {
+  const textRef = useRef(null);
+  const fileRef = useRef(null);
+
+  const [file, setFile] = useState(null);
+
+  const publishPost = async (event) => {
     event.preventDefault();
+
+    if (!textRef.current.value) return;
+
+    const posts = doc(collection(database, "posts"));
+    await setDoc(posts, {
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+      text: textRef.current.value,
+      timestamp: serverTimestamp(),
+    }).then(() => {
+      if (file) {
+        const { id } = posts;
+        const storageRef = ref(storage, "posts");
+        const thumbnailRef = ref(storageRef, id);
+        uploadString(thumbnailRef, file, "data_url").then((thumbnail) => {
+          getDownloadURL(thumbnail.ref).then((downloadURL) => {
+            const url = doc(database, "posts", id);
+            setDoc(url, { thumbnail: downloadURL }, { merge: true });
+          });
+        });
+
+        removeFile();
+      }
+    });
+
+    textRef.current.value = "";
+  };
+
+  const selectFile = (event) => {
+    const reader = new FileReader();
+
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setFile(readerEvent.target.result);
+    };
+  };
+
+  const removeFile = () => {
+    setFile(null);
   };
 
   return (
@@ -29,20 +89,35 @@ const Publisher = () => {
             type="text"
             placeholder={`What's on your mind, ${session.user.name}?`}
             className="h-12 px-5 flex-grow rounded-full bg-gray-100 focus:outline-none"
+            ref={textRef}
           />
-          <button hidden type="submit" onClick={sendPost}>
+          <button hidden type="submit" onClick={publishPost}>
             Submit
           </button>
         </form>
+        {file && (
+          <div
+            onClick={removeFile}
+            className="flex flex-col filter cursor-pointer hover:brightness-110 transition transform duration-150 hover:scale-105"
+          >
+            <img
+              src={file}
+              alt="Post Thumbnail"
+              className="h-10 object-contain"
+            />
+            <p className="text-xs text-center text-red-500">Remove</p>
+          </div>
+        )}
       </div>
       <div className="p-3 flex justify-evenly border-t">
         <div className="publisherIcon">
           <VideoCameraIcon className="h-7 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
-        <div className="publisherIcon">
+        <div onClick={() => fileRef.current.click()} className="publisherIcon">
           <CameraIcon className="h-7 text-green-400" />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          <input hidden type="file" onChange={selectFile} ref={fileRef} />
         </div>
         <div className="publisherIcon">
           <EmojiHappyIcon className="h-7 text-yellow-300" />
