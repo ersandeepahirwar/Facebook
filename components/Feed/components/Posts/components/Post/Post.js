4a7a22ddc5ef3c2ecf429f709912898b4939c6ba
ersandeepahirwar <codeysandeep@gmail.com>
2022-10-11 14:19:37 +0530

    Building the Post Component

--- components/Feed/components/Posts/components/Post/Post.js
+++ components/Feed/components/Posts/components/Post/Post.js
@@ -1,7 +1,52 @@
+import Image from "next/image";
+
+import { ChatAltIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/outline";
+
 const Post = ({ name, email, image, text, thumbnail, timestamp }) => {
   return (
-    <div>
-      <h1>Post</h1>
+    <div className="flex flex-col">
+      <div className="mt-5 p-5 bg-white rounded-t-2xl shadow-sm">
+        <div className="flex items-center space-x-2">
+          <img
+            src={image}
+            alt="User Profile"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <div>
+            <p className="font-medium">{name}</p>
+            <p className="text-xs text-gray-400">
+              {new Date(timestamp?.toDate()).toLocaleString()}
+            </p>
+          </div>
+        </div>
+        <p className="pt-4">{text}</p>
+      </div>
+      {thumbnail && (
+        <div className="relative h-56 md:h-96 bg-white">
+          <Image
+            src={thumbnail}
+            alt="Post Thumbnail"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      )}
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="postIcon rounded-none rounded-bl-2xl">
+          <ThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="postIcon rounded-none">
+          <ChatAltIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+        <div className="postIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Share</p>
+        </div>
+      </div>
     </div>
   );
 };
