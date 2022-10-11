489c26489acfa195e7ee1aa46e8b0d2e2e60d755
ersandeepahirwar <codeysandeep@gmail.com>
2022-10-11 23:49:57 +0530

    Changing Configuration of Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [require("tailwind-scrollbar-hide")],
